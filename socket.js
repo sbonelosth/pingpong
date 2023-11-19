38d4722e868866999e3e03de5b50120ba4c036dd
sbonelosth <sbonelosthole97@gmail.com>
2023-11-19 16:38:44 +0200

    some changes

--- socket.js
+++ socket.js
@@ -5,7 +5,7 @@ const onSocket = (io) => {
         socket.on("user:join", (name) => {
             !users.some((user) => user.name === name) &&
                 users.push({name, socketId: socket.id});
-            io.emit("global:message", `${name} just joined`);
+            io.emit("global:message", `${name} joined`);
         });
 
         socket.on("message:send", (payload) => {
@@ -14,7 +14,7 @@ const onSocket = (io) => {
 
         socket.on("disconnect", () => {
             const user = users.filter((user) => user.socketId === socket.id);
-            io.emit("global:message", `${user[0].name} has left`);
+            io.emit("global:message", `${user[0].name} left`);
         });
     });
 };
