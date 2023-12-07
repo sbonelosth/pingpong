c259b6ba2b16b05a877ec762e5010a0d97c27a51
sbonelosth <sbonelosthole97@gmail.com>
2023-12-07 12:42:39 +0200

    Update socket.js

--- socket.js
+++ socket.js
@@ -7,7 +7,6 @@ function createRoom() {
     var name = Math.random().toString(36).substring(7);
     // Initialize the room status with an empty array of sockets
     rooms[name] = [];
-    // Return the name of the room
     return name;
 }
 
@@ -19,11 +18,8 @@ function joinRoom(socket) {
         if (rooms[name].length < 2) {
             // Add the socket to the room array
             rooms[name].push(socket);
-            // Join the socket to the room namespace
             socket.join(name);
-            // Emit a message to the socket with the room name
             socket.emit('joined', name);
-            // Return the name of the room
             return name;
         }
     }
@@ -31,11 +27,8 @@ function joinRoom(socket) {
     var name = createRoom();
     // Add the socket to the room array
     rooms[name].push(socket);
-    // Join the socket to the room namespace
     socket.join(name);
-    // Emit a message to the socket with the room name
     socket.emit('joined', name);
-    // Return the name of the room
     return name;
 }
 
@@ -47,38 +40,33 @@ function leaveRoom(socket) {
         if (rooms[name].includes(socket)) {
             // Remove the socket from the room array
             rooms[name] = rooms[name].filter(s => s !== socket);
-            // Leave the socket from the room namespace
             socket.leave(name);
-            // Emit a message to the socket with the room name
             socket.emit('left', name);
-            // Return the name of the room
             return name;
         }
     }
-    // If the socket is not in any room, return null
+    
     return null;
 }
 
 const onSocket = (io) => {
     io.on("connection", (socket) => {
-        // Call the joinRoom function with the socket
         let room = joinRoom(socket);
         
         socket.on("user:join", (name) => {
             !users.some((user) => user.name === name) &&
                 users.push({ name, socketId: socket.id });
-            io.to(room).emit("global:message", `${name} joined`);
+            io.to(room).emit("global:message", `${name} joined room ${room}`);
         });
 
         socket.on("message:send", (payload) => {
             socket.broadcast.to(room).emit("message:receive", payload);
-            // io.to(room).emit("message:receive", payload);
         });
 
         socket.on("disconnect", () => {
             const user = users.filter((user) => user.socketId === socket.id);
             room = leaveRoom(socket);
-            io.to(room).emit("global:message", `${user[0].name} left`);
+            io.to(room).emit("global:message", `${user[0].name} left room ${room}`);
         });
     });
 };
